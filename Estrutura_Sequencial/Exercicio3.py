4cb4b752c4f15f3da4c3137f97f04dd18faa2792
Joao Paulo da Silva Monteiro <jpcabal2@hotmail.com>
2023-04-18 10:56:41 -0300

    mensagem do commit

---
+++ Estrutura_Sequencial/Exercicio3.py
@@ -0,0 +1,7 @@
+# 4. Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input('Me informe um número: '))
+numero2 = int(input('Me informe outro número: '))
+soma = numero1 + numero2
+
+print(f'A soma dos números informados é {soma}!')
---
+++ Estrutura_Sequencial/exercicio5.py
@@ -0,0 +1,6 @@
+# 5. Faça um Programa que converta metros para centímetros.
+
+metro = float(input('Me informe o valor em METRO: '))
+centrimetro = metro * 100
+
+print(f'A conversão é: {centrimetro} Cm')
